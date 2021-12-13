192a45a7d37f85a10a1cbb36dba57fd0527f42e9
Hovo <hovnaz96@gmail.com>
2021-12-13 22:01:11 +0400

    fixed submodules

---
+++ nuxt-client/src/plugin.js
@@ -0,0 +1,21 @@
+const kebabCase = require('lodash/kebabCase')
+const camelCase = require('lodash/camelCase')
+const upperFirst = require('lodash/upperFirst')
+
+export default ({ app, $axios }, inject) => {
+    inject('getcandy', {
+      async on (ref, method, ...opts) {
+        const moduleRef = `${upperFirst(camelCase(ref))}Api`
+        const apiRef = `${kebabCase(moduleRef)}`
+        try {
+          const apiModule = await import(`@poesial/js-client/dist/api/${apiRef}.js`)
+          const className = apiModule[moduleRef]
+          const module = new className(null, '', app.$auth.ctx.$axios)
+          return module[method](...opts)
+        } catch (e) {
+          throw new Error(`Unable to find ${method} on ${apiRef}`)
+        }
+      }
+    })
+
+}
